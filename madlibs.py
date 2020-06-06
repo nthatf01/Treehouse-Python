53e0fc9242ce02001bae26e0e1d8a13ff50d1275
Nathaniel Hatfield <nthatfield@gmail.com>
2020-06-06 08:12:28 -0400

    added user prompts and final output statements

--- madlibs.py
+++ madlibs.py
@@ -6,7 +6,12 @@
 #   My code is getting more __(adjective)__ every single day!
 
 
-# TODO: Prompt the user for parts of speech and store them in variables
+# Prompt the user for parts of speech and store them in variables
+madlib_verb = input("Please enter a verb: ")
+madlib_noun = input("Please enter a noun: ")
+madlib_adjective = input("Please enter an adjective: ")
 
-
-# TODO: Output the template to the screen with the blanks filled out with what the user stated
+# Output the template to the screen with the blanks filled out with what the user stated
+print("I enjoy practice! I find it helps me to", madlib_verb, "better.")
+print("Without practice, my", madlib_noun, "would probably not even work.")
+print("My code is getting more", madlib_adjective, "every single day!")
