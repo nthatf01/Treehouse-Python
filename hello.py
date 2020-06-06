b9205fa4b44eec9347afc8b1b952288d755e2bd1
Nathaniel Hatfield <nthatfield@gmail.com>
2020-06-06 07:47:48 -0400

    string variable tutorials

--- hello.py
+++ hello.py
@@ -1,1 +1,8 @@
+first_name = "Ada"
+
 print("Hello, world!")
+print("Hello, Nathaniel")
+print("Hello,", first_name)
+
+print("Nathaniel is learning Python")
+print(first_name, "is learning Python")
