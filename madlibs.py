b3fc61919733b37b99ba6c52a9862e049f739850
Nathaniel Hatfield <nthatfield@gmail.com>
2020-06-06 08:05:36 -0400

    initial commit

---
+++ madlibs.py
@@ -0,0 +1,12 @@
+# MadLibs - Practice Input and Output
+#
+# Template:
+#   I enjoy practice! I find it helps me to __(verb)__ better.
+#   Without practice, my __(noun)__ would probably not even work.
+#   My code is getting more __(adjective)__ every single day!
+
+
+# TODO: Prompt the user for parts of speech and store them in variables
+
+
+# TODO: Output the template to the screen with the blanks filled out with what the user stated
