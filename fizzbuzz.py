7d73b5d842881171a4c54b5f88ad64a2752c29cf
Nathaniel Hatfield <nthatfield@gmail.com>
2020-06-09 21:36:25 -0400

    initial commit

---
+++ fizzbuzz.py
@@ -0,0 +1,17 @@
+# Welcome to Workspaces!
+---
+Workspaces was designed to allow Treehouse Students to build and learn side by
+side with Treehouse content.
+
+
+# How to Use Workspaces
+---
+To create your first file, go to the 'File' menu and select 'New File.' Name the
+file index.html. Happy building!
+
+
+# We Want to Hear From You
+---
+Your feedback is important to us. If you have anything you want to share, good
+or bad, you can send an email to help@teamtreehouse.com.
+
