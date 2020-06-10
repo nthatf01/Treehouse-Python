2da2617dd9df556be9b54d859c97f7f0ae860df0
Nathaniel Hatfield <nthatfield@gmail.com>
2020-06-09 21:38:07 -0400

    updated with correct initial file

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,17 +1,48 @@
-# Welcome to Workspaces!
----
-Workspaces was designed to allow Treehouse Students to build and learn side by
-side with Treehouse content.
+name = input("Please enter your name: ")
+number = input("Please enter a number: ")
+
+# TODO: Make sure the number is an integer
 
 
-# How to Use Workspaces
----
-To create your first file, go to the 'File' menu and select 'New File.' Name the
-file index.html. Happy building!
+# TODO: Print out the User's name and the number entered,
+# making sure the two statements are on separate lines of output.
 
 
-# We Want to Hear From You
----
-Your feedback is important to us. If you have anything you want to share, good
-or bad, you can send an email to help@teamtreehouse.com.
+# TODO: Compare the number the user gave with the different
+# FizzBuzz conditions. 
+# *********************
+# If the number is divisible by 3, print "is a Fizz number."
+# If the number is divisible by 5, print "is a Buzz number."
+# If the number is divisible by both 3 and 5, print "is a FizzBuzz number."
+# Otherwise, print "is neither a fizzy or a buzzy number."
+# *********************
+
+
+# TODO: Define variables for is_fizz and is_buzz that stores 
+# a Boolean value of the condition. Remember that the modulo operator, %, 
+# can be used to check if there is a remainder.
+
+
+# Using the variables, check the condition of the value, and print the necessary
+# string
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
