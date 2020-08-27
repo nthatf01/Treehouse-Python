a90dc8f437bfe4748e34079ed87c7f4878879071
Nathaniel Hatfield <nthatfield@gmail.com>
2020-08-27 12:11:05 -0400

    initial commit

---
+++ team.py
@@ -0,0 +1,22 @@
+# TODO Create an empty list to maintain the player names
+
+
+# TODO Ask the user if they'd like to add players to the list.
+# If the user answers "Yes", let them type in a name and add it to the list.
+# If the user answers "No", print out the team 'roster'
+
+
+# TODO print the number of players on the team
+  
+
+# TODO Print the player number and the player name
+# The player number should start at the number one
+
+
+# TODO Select a goalkeeper from the above roster
+
+
+# TODO Print the goal keeper's name
+# Remember that lists use a zero based index
+
+
