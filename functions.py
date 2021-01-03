cbd6117a47b91c55c042077cfc78ffc80dce2aa1
Nathaniel Hatfield <nthatfield@gmail.com>
2021-01-03 16:45:59 -0500

    packing exercises

--- functions.py
+++ functions.py
@@ -18,13 +18,24 @@ def two_plus_two():
 def add_two_numbers(num1, num2):
     val = num1 + num2
     return val
-    
+
+def packer(*args):
+    for val in args:
+        print(val)
+
+def calculate_total(*args):
+    total = sum(args)
+    print(total)
 
 print_favorite_movie()
 print(num)
 
-sum = two_plus_two()
+#sum = two_plus_two()
 
-print(sum + two_plus_two())
+#print(sum + two_plus_two())
 
 print(add_two_numbers(5, 10))
+
+packer('hi', 'i', 'love', 'python')
+
+calculate_total(20, 25, 50, 100)
