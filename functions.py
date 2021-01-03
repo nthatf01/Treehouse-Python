3b94ff0f6eb95bf507bb55e8e5198403117bc156
Nathaniel Hatfield <nthatfield@gmail.com>
2021-01-03 16:05:31 -0500

    added return exercises

--- functions.py
+++ functions.py
@@ -1,5 +1,24 @@
+num = 10
+
 def print_name():
     print('Nate')
 
 def print_favorite_movie():
     print('Back to the Future')
+
+def set_num():
+    num = 5
+    letter = 'a'
+    print(letter)
+
+def two_plus_two():
+    val = 2 + 2
+    return val
+
+
+print_favorite_movie()
+print(num)
+
+sum = two_plus_two()
+
+print(sum + two_plus_two())
